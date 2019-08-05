6e1f04cb8e6305abdfe19622cf009c3061ab34eb
Toutatis <search@Toutatiss-Mac-mini.local>
2019-08-05 09:07:49 -0700

    First commit, added REST API to access items, stores, and user authentication.

---
+++ resources/item.py
@@ -0,0 +1,80 @@
+
+from flask_restful import Resource, reqparse
+from flask_jwt import jwt_required
+from models.item import ItemModel 
+
+
+class Item(Resource):
+	#cannot do this
+	#@app.route('/student/<string:name>')
+	
+	parser = reqparse.RequestParser()
+	parser.add_argument('price',
+						type = float,
+						required = True,
+						help = "This filed cannot be left blank!"
+	)
+	
+	parser.add_argument('store_id',
+						type = int,
+						required = True,
+						help = "Every item needs a store id!"
+	)
+	
+	@jwt_required()  #need authenticate before calling the jwt method. 
+	def get(self, name):
+		
+		item = ItemModel.find_by_name(name)
+		if item:
+			return item.json()		
+		return {'message' : 'Item not found'}, 404
+	
+
+	
+	def post(self, name):
+	
+		if ItemModel.find_by_name(name):
+			return {'message' : "An item with name '{}' already exists.".format(name)}, 400
+		
+		data = Item.parser.parse_args()
+		item = ItemModel(name, **data) #data['price'], data['store_id'])
+		
+		try:
+			Item.save_to_db(item)
+		except:
+			return {"message": 'An error occurred inserting the item.'}
+		
+		return item, 201   #201 created, 202 accepted, 200 OK. 
+
+
+				
+	@jwt_required()  #need authenticate before calling the jwt method. 	
+	def delete(self, name):
+
+		item = ItemModel.find_by_name(name)
+		if item:
+			item.delete_from_db()
+		return {'message': 'Item deleted'}
+		
+	def put(self, name):
+
+		data = Item.parser.parse_args()	
+		item = ItemModel.find_by_name(name)
+		
+		if item is None:
+			item = ItemModel(name, **data) #data['price'], data['store_id'])
+		else:
+			item.price = data['price']
+			
+		item.save_to_db()
+		
+		return item.json()
+		
+		
+
+class ItemList(Resource):
+
+	def get(self):
+	
+		#return {'items' : [item.json for item in ItemModel.query.all()]}
+		return {'items' : list(map(lambda x : x.json(), ItemModel.query.all()))}
